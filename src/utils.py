133bf3d9c1ca6178e94db04b8ddfbb3851a007dc
Miguel áazquez Caraballo <miguel.vazquez@oga.ai>
2024-10-16 19:00:33 +0200

    random_vector

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,14 @@ def tensor_to_image(tensor: Float[t.Tensor, "h w c"] | Int[t.Tensor, "h w c"]) -
     image = Image.fromarray(array, mode="RGB")
     return image
 
+
 @jaxtyped(typechecker=typechecker)
 def degrees_to_radians(degrees: float) -> float:
     return degrees * np.pi / 180.0
+
+
+@jaxtyped(typechecker=typechecker)
+def random_unit_vector(shape: tuple[int, ...]) -> Float[t.Tensor, "..."]:
+    vec = t.randn(*shape)
+    return vec / t.norm(vec, dim=-1, keepdim=True)
+
